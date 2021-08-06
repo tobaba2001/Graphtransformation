bfcaadd423d024f95b82372df53bc10c08ddfeea
Tobias <tobias.schrader01@gmail.com>
2021-08-06 12:26:35 +0200

    Added Test folder and fixed endless TestGraphGeneration
    The generation could potentially still be endless but there are no
    impossible graphs.

--- src/Graph.java
+++ src/Graph.java
@@ -11,44 +11,55 @@ public class Graph {
     public int E;
     public final List<List<Integer>> N;
 
+
     public Graph(int V, int E, List<List<Integer>> N) {
         this.V = V;
         this.E = E;
         this.N = N;
     }
 
-    public static Graph generate_random_Graph(int MaxNumOfVertices) {
+    public static Graph generate_random_Graph(int MaxNumOfVertices, boolean generateFile) {
         Random random = new Random();
         int V = random.nextInt(MaxNumOfVertices) + 1;
         // upper bound for edges in a graph with MaxNumOfVertices vertices:
         //      MaxNumOfVertices * ((MaxNumOfVertices - 1) / 2)
-        int E = random.nextInt(MaxNumOfVertices * ((MaxNumOfVertices - 1) / 2));
+        int E = random.nextInt(V * (V - 1)/2);
         List<List<Integer>> N = new ArrayList<>(V);
+        System.out.println("generating " + V + " vertices...");
         for (int i = 0; i < V; i++)
             N.add(new ArrayList<>());
-
+        System.out.println("generating " + E + " edges...");
         for (int i = 0; i < E; i++) {
             int a = random.nextInt(V);
             int b = random.nextInt(V);
             if ((N.get(a).contains(b)) || (a == b)) {
+                // System.out.println("going back because the edge {" + a + ", " + b + "} already exists");
+                i--;
                 continue;
             }
             // add to both neighbourhood relations, as we want to have an undirected graph
+            System.out.println("Adding edge {" + a + ", " + b + "}");
             N.get(a).add(b);
-            N.get(b).add(a);
+            // we can probably still ignore that this would give us a directed graph as we refuse to count the edges twice anyway
+             N.get(b).add(a);
         }
         Graph G = new Graph(V, E, N);
-        String filename = "Graph-" + System.currentTimeMillis() + ".txt";
-        Path path = Paths.get(filename);
-        try {
-            Files.writeString(path, G.toString(), Charset.defaultCharset(),  StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-        } catch (IOException e) {
-            System.out.println("Could not save orders.\n");
-            e.printStackTrace();
+        if (generateFile) {
+            String filename = "Graph-" + System.currentTimeMillis() + ".txt";
+            Path path = Paths.get(filename);
+            try {
+                Files.writeString(path, G.toString(), Charset.defaultCharset(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+            } catch (IOException e) {
+                System.out.println("Could not save graph.\n");
+                e.printStackTrace();
+            }
+            System.out.println("Graph saved to: " + path);
         }
-        System.out.println("Orders saved to: " + path);
         // NOTE: N does not give us the edges but rather the relation of Neighbourhood between vertices.
         return G;
+    }
+    public static Graph generate_random_Graph(int i) {
+        return generate_random_Graph(i, false);
     }
 
     @Override
@@ -79,10 +90,5 @@ public class Graph {
         builder.append("]");
         finalString = builder.toString();
         return finalString;
-    }
-
-    public static void main(String[] args) {
-        Graph G = generate_random_Graph(100);
-        System.out.println(G);
     }
 }
---
+++ test/TestGraphGeneration.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static java.time.Duration.ofSeconds;
+import static java.time.Duration.ofMinutes;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class TestGraphGeneration {
+    Graph graph;
+
+    @BeforeEach
+    void init() {
+        //graph = assertTimeoutPreemptively(ofMinutes(1), () -> Graph.generate_random_Graph(1000, true));
+        graph = Graph.generate_random_Graph(1000, true);
+    }
+    @Test
+    void TestNumberOfEdges(){
+        List<Set<Integer>> edges = new ArrayList<>();
+        for (int i = 0; i < graph.V; i++){
+            for(Integer j : graph.N.get(i)) {
+                Set<Integer> tmp = new HashSet<>();
+                tmp.add(i);
+                tmp.add(j);
+                if (!edges.contains(tmp)){
+                    edges.add(tmp);
+                }
+            }
+        }
+        assertEquals(edges.size(),
+                graph.E,
+                "testing whether the said number of edges is actually represented in the graph");
+    }
+}
